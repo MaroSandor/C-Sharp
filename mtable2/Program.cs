aff0a5d9eb7d2bda8102c703f7320c6ad76549bc
MaroSandor <2021298@dszcberegszaszi.hu>
2022-02-04 11:38:24 +0100

    0204_2

---
+++ mtable2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mtable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Két parancssori paraméter is megadható. Parancssori paraméterek száma lehet: 0, 1, 2 */
+            int param = args.Length; // Paraméterek száma
+            int szam1 = 0; // 1. paraméter értéke
+            int szam2 = 0; // 2. paraméter értéke
+
+            Random rnd = new Random();
+
+            switch(param)
+            {
+                case 0:
+                    szam1 = rnd.Next(1, 100);
+                    szam2 = rnd.Next(1, 100);
+                    break;
+                case 1:
+                    szam1 = int.Parse(args[0]);
+                    szam2 = rnd.Next(1, 100);
+                    break;
+                case 2:
+                    szam1 = int.Parse(args[0]);
+                    szam2 = int.Parse(args[1]);
+                    break;
+            }
+
+            // Kiíratom az eredményt
+            Console.WriteLine(szam1 * szam2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ szamologep/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szamologep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam1;
+            int szam2;
+            char muvelet;
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Adja meg az első számot!");
+                szam1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Adja meg a második számot!");
+                szam2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Adja meg a műveleti jelet!");
+                muvelet = Convert.ToChar(Console.Read());
+            }
+            else
+            {
+                if (args.Length != 3)
+                {
+                    Console.WriteLine("Túl kevés a paraméter!");
+                    return;
+                }
+                else
+                {
+                    szam1 = int.Parse(args[0]);
+                    szam2 = int.Parse(args[1]);
+                    muvelet = Convert.ToChar(args[2]);
+                }
+            }
+
+            int ertek = 0;
+
+            switch (muvelet)
+            {
+                case '+':
+                    ertek = szam1 + szam2;
+                    break;
+                case '-':
+                    ertek = szam1 - szam2;
+                    break;
+                case '*':
+                    ertek = szam1 * szam2;
+                    break;
+                case '/':
+                    ertek = szam1 / szam2;
+                    break;
+                default:
+                    Console.WriteLine("Hibás műveleti jel.");
+                    break;
+            }
+            Console.WriteLine("Az eredmény: {0}", ertek);
+
+            Console.ReadKey();
+        }
+    }
+}
