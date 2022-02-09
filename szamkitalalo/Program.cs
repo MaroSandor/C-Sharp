dc0b945ac0e4684218e8bc409763f5c7e5b4ec68
MaroSandor <2021298@dszcberegszaszi.hu>
2022-02-09 11:40:21 +0100

    1140update

--- szamkitalalo/Program.cs
+++ szamkitalalo/Program.cs
@@ -10,63 +10,103 @@ namespace szamkitalalo
     {
         static void Main(string[] args)
         {
-        START:
+            //    int i = 0;
+            //    Random randomszam = new Random();
+            //    int szam = randomszam.Next(100);
+            //START:
 
-            Console.WriteLine("Válassz!");
-            Console.WriteLine("1 - Játékosmód");
-            Console.WriteLine("2- Számítógépmód");
+            //    Console.WriteLine("Válassz!");
+            //    Console.WriteLine("1 - Játékosmód");
+            //    Console.WriteLine("2- Számítógépmód");
 
-            switch (Console.ReadKey(true).KeyChar)
-            {
-                case '1': goto PLAYER;
-                case '2': goto PC;
-            }
+            //    switch (Console.ReadKey(true).KeyChar)
+            //    {
+            //        case '1': goto PLAYER;
+            //        case '2': goto PC;
+            //    }
 
-        PLAYER: goto JATEKOS;
-        PC: goto PC;
+            //    PLAYER: goto JATEKOS;
+            //    PC: goto AI;
 
-        JATEKOS:
+            //JATEKOS:
 
-            Console.WriteLine("Akarsz még játszani? i/n");
+            //    Console.WriteLine("Gondolj egy számra! (1-100)");
+            //    int kitalalt = int.Parse(Console.ReadLine());
 
-            switch (Console.ReadKey(true).KeyChar)
-            {
-                case 'n': break;
-                default: goto JATEKOS;
-            }
+            //    int min = 0;
+            //    int max = 100;
 
-            Console.WriteLine("Gondolj egy számra! (1-100)");
-            int kitalalt = int.Parse(Console.ReadLine());
+            //    while (i < 5) {
+            //        Console.WriteLine("A számítógép szerint a szám: {0}", szam);
+            //        Console.WriteLine("Kisebb, nagyobb vagy egyenlő? (k/n/e)");
 
-            Random r = new Random();
+            //        switch (Console.ReadKey().KeyChar)
+            //        {
+            //            case 'k':
+            //                Console.WriteLine("A szám kisebb!");
+            //                szam = randomszam.Next(min, kitalalt);
+            //                break;
+            //            case 'n':
+            //                Console.WriteLine("A szám nagyobb!");
+            //                szam = randomszam.Next(kitalalt, max);
+            //                break;
+            //            case 'e':
+            //                Console.WriteLine("A gép eltalálta!");
+            //                break;
+            //        }
+            //        i++;
+            //    }
 
-            int min = 0;
-            int max = 100;
+            //    Console.WriteLine("Akarsz még játszani? i/n");
+
+            //    switch (Console.ReadKey(true).KeyChar)
+            //    {
+            //        case 'n': break;
+            //        default: goto JATEKOS;
+            //    }
+
+            //    Console.ReadKey();
+
+            //AI:
+
+            //    Console.WriteLine("Gondoltam egy számra!");
+            //    Console.WriteLine("A tipped:");
+            //    int tipp = Console.ReadKey().KeyChar;
+
+            //    if (tipp < szam)
+            //    {
+            //        Console.WriteLine("A szám nagyobb!");
+            //    } else if (szam < tipp)
+            //    {
+            //        Console.WriteLine("A szám kisebb!");
+            //    } else
+            //    {
+            //        Console.WriteLine("Találat!");
+            //    }
+            // Alapszerkezet
+
             int i = 0;
-            int szam = r.Next(100);
+            Random pcSzam = new Random();
 
-            while (i < 5) {
-                Console.WriteLine("A számítógép szerint a szám: {0}", szam);
-                Console.WriteLine("Kisebb, nagyobb vagy egyenlő? (k/n/e)");
-                
-                switch (Console.ReadKey().KeyChar)
-                {
-                    case 'k':
-                        Console.WriteLine("A szám kisebb!");
-                        szam = r.Next(min, kitalalt);
-                        break;
-                    case 'n':
-                        Console.WriteLine("A szám nagyobb!");
-                        szam = r.Next(kitalalt, max);
-                        break;
-                    case 'e':
-                        Console.WriteLine("A gép eltalálta!");
-                        break;
-                }
-                i++;
-                Console.ReadKey();
+            Console.WriteLine("Válassz a játkmódok közül!");
+            Console.WriteLine("1 - Játékos mód");
+            Console.WriteLine("2 - Gépi mód");
+            int mod = Console.ReadKey(true).KeyChar;
+
+            if (mod == 1)
+            {
+
             }
-            
+            else if (mod == 2)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Ilyen játékmód nincs!");
+            }
+
+            Console.ReadKey();
         }
     }
 }
