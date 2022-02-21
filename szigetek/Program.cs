fba03021a49796b457947f4e8e17c3fddf6891d8
Sándor Péter Máró <2021298@dszcberegszaszi.hu>
2022-02-21 14:24:56 +0100

    update

--- szigetek/Program.cs
+++ szigetek/Program.cs
@@ -10,6 +10,34 @@ namespace szigetek
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Adja meg a karaktersorozatot!");
+            string data = Console.ReadLine();
+            int islandCount = 0;
+            int maxIslandLength = 0;
+            int i = 0;
+            while (i < data.Length)
+            {
+                if (data[i] == '1')
+                {
+                    ++islandCount;
+                    int j = i;
+                    int tmp = 0;
+                    while (j < data.Length && data[j] == '1')
+                    {
+                        ++j;
+                        ++tmp;
+                    }
+                    i = j;
+                    if (tmp > maxIslandLength) { maxIslandLength = tmp; }
+                }
+                else
+                {
+                    ++i;
+                }
+            }
+            Console.WriteLine("A szigetek száma: {0}", islandCount);
+            Console.WriteLine("A leghosszabb sziget hossza: {0} km", maxIslandLength);
+            Console.ReadKey();
         }
     }
 }
