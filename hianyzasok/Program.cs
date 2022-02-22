16b660ad70b1edc07b0f5e615598322120dbb7dd
Sándor Péter Máró <2021298@dszcberegszaszi.hu>
2022-02-22 11:41:26 +0100

    update

--- hianyzasok/Program.cs
+++ hianyzasok/Program.cs
@@ -11,9 +11,44 @@ namespace hianyzasok
         static void Main(string[] args)
         {
             // Változók deklarálása
-            int igazolatlan;
+            int igazolatlan,
+                magatartasjegy;
             DateTime szuletesidatum;
-            string[] buntetesmertek = new string[] { };
+            int[] buntetesmertek = new int[] {1, 2, 3, 4, 5};
+
+            Console.WriteLine("Kérem adja meg a tanuló hiányzásainak számát!");
+            igazolatlan = int.Parse(Console.ReadLine());
+
+            // Magatartás jegy meghatározása
+
+            if (igazolatlan <= 1)
+            {
+                magatartasjegy = buntetesmertek[4];
+            } else if (igazolatlan <= 3)
+            {
+                magatartasjegy = buntetesmertek[3];
+            } else if (igazolatlan <= 5)
+            {
+                magatartasjegy = buntetesmertek[2];
+            } else if (igazolatlan <= 7) {
+                magatartasjegy = buntetesmertek[1];
+            } else
+            {
+                magatartasjegy = buntetesmertek[0];
+            }
+
+            Console.WriteLine("A magatartásod jegye: {0}", magatartasjegy);
+
+            // Igazolatlan hiányzások bekérése
+
+            
+            if (igazolatlan >= 10)
+            {
+                Console.WriteLine("Kérem adja meg a születési dátumát! (YYYY.MM.DD.)");
+                szuletesidatum = Convert.ToDateTime(Console.ReadLine());
+                Console.WriteLine("Az igazolatlan hiányzások száma: {0}. \nSzületési dátuma: {1}", igazolatlan, szuletesidatum);
+            }
+            Console.ReadKey();
         }
     }
 }
