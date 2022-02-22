c7b34580f3a7704880b131ef48a0597fa7136c3b
Sándor Péter Máró <2021298@dszcberegszaszi.hu>
2022-02-22 15:26:02 +0100

    0

--- cimletezes/Program.cs
+++ cimletezes/Program.cs
@@ -10,6 +10,24 @@ namespace cimletezes
     {
         static void Main(string[] args)
         {
+            // Változók
+            int huszezer = 0,
+                tizezer = 0,
+                otezer = 0,
+                ketezer = 0,
+                ezer = 0,
+                otszaz = 0,
+                ketszaz = 0,
+                szaz = 0,
+                otven = 0,
+                husz = 0,
+                tiz = 0,
+                ot = 0,
+                penzertek;
+
+            Console.WriteLine("Írja be az összeget!");
+            penzertek = int.Parse(Console.ReadLine());
+
 
         }
     }
