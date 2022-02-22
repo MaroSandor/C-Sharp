91ba3e80fb15825884aa65b7b2e1d6d935d40aeb
Sándor Péter Máró <2021298@dszcberegszaszi.hu>
2022-02-22 15:01:43 +0100

    csempezes

--- csempezes/Program.cs
+++ csempezes/Program.cs
@@ -17,18 +17,18 @@ namespace csempezes
                 darabszam,
                 csempemeret;
 
-            Console.WriteLine("Kérem a szoba szélességét!");
+            Console.WriteLine("Kérem a szoba szélességét! (méter)");
             szel = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Kérem a szoba mélységét!");
+            Console.WriteLine("Kérem a szoba mélységét! (méter)");
             mely = Convert.ToDouble(Console.ReadLine());
 
             csempemeret = Math.Pow(0.2, 2);
 
             terulet = szel * mely;
 
-            darabszam = terulet / csempemeret;
+            darabszam = (terulet / csempemeret) * 1.1;
 
-            Console.WriteLine("A szükséges csempék száma: {0}", darabszam);
+            Console.WriteLine("A szükséges csempék száma: {0} db.", darabszam);
             Console.ReadKey();
         }
     }
