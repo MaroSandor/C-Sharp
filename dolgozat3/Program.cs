0f3fbc403a09467798835c23095ca2bb01d37432
Sándor Péter Máró <2021298@dszcberegszaszi.hu>
2022-02-23 09:41:02 +0100

    dolgozat_3

--- dolgozat3/Program.cs
+++ dolgozat3/Program.cs
@@ -10,6 +10,37 @@ namespace dolgozat3
     {
         static void Main(string[] args)
         {
+            string nev,
+                foglalkozas;
+            char nem;
+            int szerencse;
+
+            Random rnd = new Random();
+
+            
+
+            do
+            {
+                Console.Write("Add meg a nevet! ");
+                nev = Console.ReadLine();
+                Console.Write("Add meg a foglalkozását! ");
+                foglalkozas = Console.ReadLine();
+                Console.Write("Add meg a nemét! (f/n) ");
+                nem = Convert.ToChar(Console.ReadLine());
+
+                szerencse = rnd.Next(1, 50);
+
+                if (nem == 'f')
+                {
+                    Console.WriteLine("{0} egy {1} férfi volt, szerencse száma a {2}.", nev, foglalkozas, szerencse);
+                } else if (nem == 'n')
+                {
+                    Console.WriteLine("{0} egy {1} nő volt, szerencse száma a {2}.", nev, foglalkozas, szerencse);
+                }
+            } while (nev != "");
+            
+
+            Console.ReadKey();
         }
     }
 }
