2f1b147ed62a319a151ac1a30c6b9a9d10f7f89b
Sándor Péter Máró <2021298@dszcberegszaszi.hu>
2022-02-23 10:25:48 +0100

    2dolgozat_2

--- 2dolgozat_2/Program.cs
+++ 2dolgozat_2/Program.cs
@@ -10,7 +10,22 @@ namespace _2dolgozat_2
     {
         static void Main(string[] args)
         {
-            
+            // Változók
+            int szam = 0,
+                szam2 = 0;
+
+            do
+            {
+                Console.WriteLine("Kérek egy számot!");
+                szam = int.Parse(Console.ReadLine());
+                if (szam > szam2)
+                {
+                    szam2 = szam;
+                } else
+                {
+                    Console.WriteLine("A legnagyobb szám amit beírtál a: {0}.", szam2);
+                }
+            } while (szam != 0);
 
             Console.ReadKey();
         }
