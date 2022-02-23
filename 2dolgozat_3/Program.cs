1035ad444eb94c634fe305f01e8a577f80945bbf
Sándor Péter Máró <2021298@dszcberegszaszi.hu>
2022-02-23 10:37:00 +0100

    2dolgozat_3

---
+++ 2dolgozat_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2dolgozat_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,
+                b,
+                ertek = 0;
+
+            Console.WriteLine("Adja meg az intervallum kezdőértékét!");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Adja meg az intervallum végőértékét!");
+            b = int.Parse(Console.ReadLine());
+
+            for (int i = a; i <= b; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine("5-tel osztható számok: {0}", i);
+                }
+            }            
+
+            Console.ReadKey();
+
+        }
+    }
+}
