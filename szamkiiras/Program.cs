8e50277ec0b336b468aaf9f63ff368da49b92396
MaroSandor <marosandor2001@gmail.com>
2022-02-23 15:54:14 +0100

    hülyeség

--- szamkiiras/Program.cs
+++ szamkiiras/Program.cs
@@ -10,6 +10,22 @@ namespace szamkiiras
     {
         static void Main(string[] args)
         {
+            string szam = "";
+            var egyes = new string[] { "nulla", "egy", "kettő", "három", "négy", "öt", "hat", "hét", "nyolc", "kilenc" };
+            var tizes = new string[] { "tizen", "huszon", "harminc", "negyven", "ötven", "hatvan", "hetven", "nyolcvan", "kilencven", "száz" };
+
+            Console.WriteLine("Kérem írjon be egy számot!");
+            szam = Console.ReadLine();
+
+            for (int i = 0; i < szam.Length; i++)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine(egyes[i]);
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
