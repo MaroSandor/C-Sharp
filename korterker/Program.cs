37273803af7e5c4bf4d9ca7d862a9c98209c68c9
Sándor Péter Máró <2021298@dszcberegszaszi.hu>
2022-03-04 10:45:27 +0100

    korosztaly

---
+++ korterker/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace korterker
+{
+    class Kor
+    {
+        // Osztályváltozók
+        private int r;
+        private double terulet,
+            kerulet;
+
+        public Kor() { }
+
+        public Kor(int r, double terulet, double kerulet)
+        {
+            this.r = r;
+            this.terulet = terulet;
+            this.kerulet = kerulet;
+        }
+
+        public double terszamitas()
+        {
+            this.terulet = Math.Pow(getBekeres(), 2) * Math.PI;
+            return this.terulet;
+        }
+
+        public double kerszamitas ()
+        {
+            this.kerulet = 2 * getBekeres() * Math.PI;
+            return this.kerulet;
+        }
+
+        public void setBekeres()
+        {
+            int ertek = Convert.ToInt32(Console.ReadLine());
+            if (ertek != 0)
+            {
+                this.r = ertek;
+            }
+        }
+
+        public double getBekeres()
+        {
+            return this.r;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adja meg a sugarat: ");
+            Kor a = new Kor();
+            a.setBekeres();
+            Console.WriteLine("A kör sugara: {0}, területe: {1}, a kör kerülete: {2}.", a.getBekeres(), a.terszamitas(), a. kerszamitas());
+            Console.ReadKey();
+        }
+    }
+}
