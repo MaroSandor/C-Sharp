a392c4aeb5a9f33faf2301d49144951e2cddcc98
Sándor Péter Máró <2021298@dszcberegszaszi.hu>
2022-03-09 11:38:50 +0100

    up

--- matrixatlo/Program.cs
+++ matrixatlo/Program.cs
@@ -12,7 +12,7 @@ namespace matrixatlo
 
         public Matrix()
         {
-        https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ictdemy.com%2Fcsharp%2Fbasics%2Fmultidimensional-arrays-in-csharp-net&psig=AOvVaw2ov8FnMmiTQub8lrb4iqag&ust=1646907576504000&source=images&cd=vfe&ved=0CAkQjhxqFwoTCJDoxPrmuPYCFQAAAAAdAAAAABAU
+
         }
 
     }
