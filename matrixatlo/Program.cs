5198f791fa1a6a876fbf9398798e333038b62727
Sándor Péter Máró <2021298@dszcberegszaszi.hu>
2022-03-09 11:23:48 +0100

    link

---
+++ matrixatlo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matrixatlo
+{
+    class Matrix
+    {
+        // Osztályváltozók
+
+        public Matrix()
+        {
+        https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ictdemy.com%2Fcsharp%2Fbasics%2Fmultidimensional-arrays-in-csharp-net&psig=AOvVaw2ov8FnMmiTQub8lrb4iqag&ust=1646907576504000&source=images&cd=vfe&ved=0CAkQjhxqFwoTCJDoxPrmuPYCFQAAAAAdAAAAABAU
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
