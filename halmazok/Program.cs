d54bddd82fc52b3b87eb776be187ce5e87b2369f
Sándor Péter Máró <2021298@dszcberegszaszi.hu>
2022-03-18 11:39:28 +0100

    halmazok

--- halmazok/Program.cs
+++ halmazok/Program.cs
@@ -21,21 +21,115 @@ namespace halmazok
     {
         // Osztályváltozók
         private int elemekszama = 0,
+            metszet2,
             elemszam;
         private int[] tombA,
+            metszet,
+            unio,
+            AB,
+            BA,
             tombB;
+        private bool jeloles = false;
 
-        public Halmazok()
+        public Halmazok() { }
+
+        public void Feltoltes()
         {
-            this.elemszam = elemekszama;
-            this.tombA = tombA;
-            this.tombB = tombB;
+            Random rnd = new Random();
+            Console.WriteLine("Adja meg a tömb elemeinek számát:");
+            elemszam = Convert.ToInt32(Console.ReadLine());
+            if (elemszam > 200)
+            {
+                elemszam = rnd.Next(100, 200);
+            }
+            this.tombA = new int[elemszam];
+            this.tombB = new int[elemszam];
+            this.metszet = new int[elemszam * 2];
+            this.unio = new int[elemszam * 2];
+            this.AB = new int[elemszam];
+            this.BA = new int[elemszam];
+            for (int i = 0; i < elemszam; i++)
+            {
+                this.tombA[i] = rnd.Next(-100, 100);
+                this.tombB[i] = rnd.Next(-100, 100);
+            }
+            Console.WriteLine("Az A halmaz elemei:");
+            for (int i = 0; i < tombA.Length; i++)
+            {
+                Console.Write("{0} ", tombA[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("A B halmaz elemei:");
+            for (int i = 0; i < tombB.Length; i++)
+            {
+                Console.Write("{0} ", tombB[i]);
+            }
+        }
+
+        public void Metszet()
+        {
+            int k = 0;
+            for (int i = 0; i < this.tombA.Length; i++)
+            {
+                for (int j = 0; j < this.tombB.Length; j++)
+                {
+                    if (this.tombA[i] == this.tombB[j])
+                    {
+                        this.metszet[k] = this.tombA[i];
+                        k++;
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("A metszet elemei: ");
+            for (int l = 0; l < k; l++)
+            {
+                Console.Write("{0} ", this.metszet[l]);
+            }
+            this.jeloles = true;
+        }
+
+        public void ABhalmaz()
+        {
+            if (!this.jeloles)
+            {
+                Metszet();
+            }
+            int k = 0;
+            bool paros = false;
+            for (int i = 0; i < this.tombA.Length; i++)
+            {
+                paros = false;
+                for (int j = 0; j < this.metszet.Length; j++)
+                {
+                    if (this.tombA[i] == this.metszet[i])
+                    {
+                        paros = true;
+                        break;
+                    }
+                }
+                if (!paros)
+                {
+                    this.AB[k] = this.tombA[i];
+                    k++;
+                }
+            }
+            Console.WriteLine("\nA\\B halmaz:");
+            for (int x = 0; x < k; x++)
+            {
+                Console.Write("{0} ", this.AB[x]);
+            }
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
+            Halmazok a = new Halmazok();
+            a.Feltoltes();
+            a.Metszet();
+            a.ABhalmaz();
+            Console.ReadKey();
         }
     }
 }
