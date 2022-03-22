01a44f2132889d0065bc4d4749007a574b637824
Sándor Péter Máró <2021298@dszcberegszaszi.hu>
2022-03-22 14:05:53 +0100

    időjárás

--- idojaras/Program.cs
+++ idojaras/Program.cs
@@ -9,6 +9,36 @@ namespace idojaras
     class Idojaras
     {
         // Osztályváltozók
+        private int ev,
+            nap,
+            napok,
+            homerseklet;
+        private string honap,
+            honapneve,
+            evszak,
+            evszakok;
+        private string[] honapok = new string[] {"január", "február", "március", "április", "május", "június", "július", "augusztus", "szeptember", "október", "november", "december"};
+
+        public Idojaras() {
+            this.evszakok = evszak;
+            this.honapneve = honap;
+            this.napok = nap;
+        }
+
+        public void datumBekeres()
+        {
+            Console.Write("Kérem adja meg az évet: ");
+            ev = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Kérem adja meg a hónapot: ");
+            honap = Console.ReadLine();
+            Console.Write("Kérem adja meg a napot: ");
+            nap = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public int getHom()
+        {
+            return homerseklet;
+        }
     }
 
     class Program
@@ -16,7 +46,8 @@ namespace idojaras
         static void Main(string[] args)
         {
             // Meghívások
-
+            Idojaras osztaly = new Idojaras();
+            osztaly.datumBekeres();
             Console.ReadKey();
         }
     }
