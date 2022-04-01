13eaba12401addbf8fdc8f529120362bbbcdba50
Sándor Péter Máró <2021298@dszcberegszaszi.hu>
2022-04-01 10:16:13 +0200

    Kettes

---
+++ Kettes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kettes
+{
+    class Kettes
+    {
+        private int bekertszam;
+        private string atvaltott;
+
+        public Kettes() { }
+
+        public void atvalto()
+        {
+            Console.WriteLine("Adja meg az átváltandó tízes számrendszerbeli számot:");
+            bekertszam = Convert.ToInt32(Console.ReadLine());
+            atvaltott = Convert.ToString(bekertszam, 2);
+            Console.WriteLine("A kettes számrendszerbeli szám:");
+            Console.Write(atvaltott);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kettes a = new Kettes();
+            a.atvalto();
+            Console.ReadKey();
+        }
+    }
+}
