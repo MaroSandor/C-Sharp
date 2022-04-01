5b923bde026a602ab76406b1b9a73d746c6387a8
Sándor Péter Máró <2021298@dszcberegszaszi.hu>
2022-04-01 09:02:42 +0200

    Binaris

---
+++ Binaris/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binaris
+{
+    class Binaris
+    {
+        public Binaris() { }
+
+        public void szamolas()
+        {
+            string szam;
+            int egyes = 0;
+            
+            Console.WriteLine("Kérem adjon meg egy bináris számot:");
+            szam = Console.ReadLine();
+
+            for (int i = 0; i < szam.Length; i++)
+            {
+                if (szam[i] == '1')
+                {
+                    egyes++;
+                }
+            }
+            Console.WriteLine("Az egyesek száma: {0}", egyes);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Binaris a = new Binaris();
+            a.szamolas();
+            Console.ReadKey();
+        }
+    }
+}
