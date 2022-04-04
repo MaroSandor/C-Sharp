46154b605de965b9f366ee4a87f285fa09bfef23
Sándor Péter Máró <2021298@dszcberegszaszi.hu>
2022-04-04 14:22:37 +0200

    Nullak

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Fibonacci
+    {
+        private int generalando,
+            szam;
+        private List<ulong> fibonacci = new List<ulong> ();
+
+        public Fibonacci()
+        {
+            this.szam = generalando;
+        }
+
+        public void bekeres()
+        {
+            Console.WriteLine("Adja meg, hogy hány Fibonacci számot szeretne kiírni:");
+            generalando = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void sorozat()
+        {
+            ulong kezdo = 0;
+            for (int i = 0; i < this.szam; i++)
+            {
+                fibonacci.Add();
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci osztaly = new Fibonacci();
+            osztaly.bekeres();
+            osztaly.sorozat();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Nullak/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullak
+{
+    class Nullak
+    {
+        private int db_szam = 0;
+        Random szamok = new Random();
+
+        public Nullak() { }
+
+        public void eloallitas()
+        {
+            Console.WriteLine("==========================================");
+            Console.WriteLine("   Adja meg a generálandó számok számát");
+            Console.WriteLine("==========================================");
+            db_szam = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            for (int i = 0; i < db_szam; i++)
+            {
+                int a = szamok.Next(100000, 999999);
+                Console.WriteLine("{0}", Convert.ToString(a));
+
+                for (int j = 0; j < i; j++)
+                {
+
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullak osztaly = new Nullak();
+            osztaly.eloallitas();
+            Console.ReadKey();
+        }
+    }
+}
