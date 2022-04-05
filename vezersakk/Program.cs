a2ef857490c78d3b5d2ba46d559b260dd8e682c5
Sándor Péter Máró <2021298@dszcberegszaszi.hu>
2022-04-05 12:18:18 +0200

    vezersakk

---
+++ vezersakk/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vezersakk
+{
+    class Sakk
+    {
+        private int[] xtengely = new int[] {1, 2, 3, 4, 5, 6, 7, 8};
+        private int[] ytengely = new int[] {1, 2, 3, 4, 5, 6, 7, 8};
+        private int pos_a,
+            pos_b;
+
+        public Sakk() { }
+
+        public void elhelyezes()
+        {
+            for (int i = 0; i < xtengely.Length; i++)
+            {
+                for (int j = 0; j < ytengely.Length; j++)
+                {
+                    if (i == 0 && j == 0)
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
