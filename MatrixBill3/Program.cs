c99f82a34ffdee1bc8aa90cf624a676946841dab
Sándor Péter Máró <2021298@dszcberegszaszi.hu>
2022-04-20 09:27:55 +0200

    MatrixBill

---
+++ MatrixBill3/Program.cs
@@ -0,0 +1,62 @@
+﻿// Máró Sándor
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixBill
+{
+    class Matrix
+    {
+        private int sor,
+            oszlop;
+        private int[,] tomb;
+
+        public Matrix() { }
+
+        public void bekeres()
+        {
+            Console.WriteLine("Adja meg a mátrix sorainak számát:");
+            sor = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Adja meg a mátrix oszlopainak számát:");
+            oszlop = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Adja meg az elemeket:");
+
+            tomb = new int[sor, oszlop];
+
+            for (int i = 0; i < tomb.GetLength(0); i++)
+            {
+                for (int j = 0; j < tomb.GetLength(1); j++)
+                {
+                    tomb[i, j] = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("A {0} elem a {1}, {2} helyen áll.", tomb[i, j], i, j);
+                }
+            }
+        }
+
+        public void kiiratas()
+        {
+            for (int i = 0; i < sor; i++)
+            {
+                for (int j = 0; j < oszlop; j++)
+                {
+                    Console.Write("{0, 4} ", tomb[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix osztaly = new Matrix();
+            osztaly.bekeres();
+            osztaly.kiiratas();
+            Console.ReadKey();
+        }
+    }
+}
