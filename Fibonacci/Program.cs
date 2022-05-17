c33917c38d63e62b7e43116b87fd9e47c05b11c3
Sándor Péter Máró <2021298@dszcberegszaszi.hu>
2022-05-17 15:23:39 +0200

    FibonacciTeszt

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -6,42 +6,54 @@ using System.Threading.Tasks;
 
 namespace Fibonacci
 {
-    class Fibonacci
+    public class Fibonaci
     {
-        public Fibonacci() { }
-
         private int szam,
             a = 0,
-            b = 1;
+            b = 1,
+            osszeg;
         private int[] fibonacciszamok;
 
-        public void feltoltes()
+        public Fibonaci()
+        {
+            this.szam = szam;
+            this.osszeg = osszeg;
+        }
+
+        public int feltoltes(int szam)
         {
             Console.Write("Kérem adja meg, hogy hány számot szeretne kiíratni: ");
             szam = Convert.ToInt32(Console.ReadLine());
 
-            fibonacciszamok = new int[szam];
+            fibonacciszamok = new int[this.szam];
 
-            Console.WriteLine("A Fibonacci-sorozat számai {0}-ig:", szam);
+            Console.WriteLine("A Fibonacci-sorozat számai {0}-ig:", this.szam);
             Console.Write("{0} {1} ", a, b);
 
-            for (int i = 1; i < szam; i++)
+            for (int i = 1; i < this.szam; i++)
             {
-                fibonacciszamok[i] += fibonacciszamok[i - 1] + fibonacciszamok[i];
+                fibonacciszamok[i] =+ fibonacciszamok[i - 1] + fibonacciszamok[i];
             }
 
             foreach (var item in fibonacciszamok)
             {
                 Console.WriteLine(item);
             }
+
+            foreach (var item in fibonacciszamok)
+            {
+                osszeg =+ item;
+            }
+
+            return this.osszeg;
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Fibonacci osztaly = new Fibonacci();
-            osztaly.feltoltes();
+            Fibonaci osztaly = new Fibonaci();
+            osztaly.feltoltes(Convert.ToInt32(Console.ReadLine()));
             Console.ReadKey();
         }
     }
---
+++ FibonacciTeszt/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Fibonacci;
+
+namespace FibonacciTeszt
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Fibonaci ok = new Fibonaci();
+
+        [TestMethod]
+        public void sorozatteszt()
+        {
+            int varteredmeny = 1;
+
+            int kapotteredmeny = ok.feltoltes(5);
+
+            Assert.AreEqual(varteredmeny, kapotteredmeny);
+        }
+    }
+}
