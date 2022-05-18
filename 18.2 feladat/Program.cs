b62e73f9c0933bc1ca5fdab139c1e040a6ff6572
Sándor Péter Máró <2021298@dszcberegszaszi.hu>
2022-05-18 13:13:15 +0200

    hiányzó feladat

---
+++ 18.2 feladat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _18._2_feladat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ KAR18.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KAR18._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Az előző feladatban beolvasott listátjelenítsük meg a képernyőn táblázatos, lapozható formában!A lapozást előre-hátra mó - don oldjuk meg, képernyőnként 20 rekord kiírással kalkulálva!
+            //A lapozást a PageUp ésPageDown gombokkal lehessen megvalósítani!Ezenfelül a Home billentyűvel lehessenaz első, az End billentyűvel az utolsó lapra ugorni!2Segítség a megoldáshoz:
+            //A lista beolvasását a18.1.feladatban leírtak szerint végezhetjük el.Vezessünk be egy változót, mely annak sorszámát tartalmazza, hogy hanyadik elemtől kezdő-dik a kiírás(ez kezdetben az első barát, 0 sorszámtól indul)!
+            //Készítsünk egy kiíró függvényt,mely a lista adott kezdő indexétől kezdve ír ki 20 rekordot(ha van annyi egyáltalán hátra)!
+            //A billentyűzetkezeléssel kapcsolatosan a10.11.feladat kapcsán már írtunk, így minden szükségesismeret rendelkezésre áll, hogy megoldhassuk a problémát.
+        }
+    }
+}
