749a0e3aa0219c4a44445089db26f45d007e9874
Sándor Péter Máró <2021298@dszcberegszaszi.hu>
2022-05-26 10:50:28 +0200

    LabelSzoveg

--- LabelSzoveg/LabelSzoveg.cs
+++ LabelSzoveg/LabelSzoveg.cs
@@ -31,6 +31,28 @@ namespace LabelSzoveg
         {
             int o = (int)e.X / (lblFull.Width / 3); // oszlop
             int s = (int)e.Y / (lblFull.Height / 3); // sor
+
+            switch (s * 3 + o)
+            {
+                case 0:
+                    lblFull.TextAlign = ContentAlignment.TopLeft; break;
+                case 1:
+                    lblFull.TextAlign = ContentAlignment.TopCenter; break;
+                case 2:
+                    lblFull.TextAlign = ContentAlignment.TopRight; break;
+                case 3:
+                    lblFull.TextAlign = ContentAlignment.MiddleLeft; break;
+                case 4:
+                    lblFull.TextAlign = ContentAlignment.MiddleCenter; break;
+                case 5:
+                    lblFull.TextAlign = ContentAlignment.MiddleRight; break;
+                case 6:
+                    lblFull.TextAlign = ContentAlignment.BottomLeft; break;
+                case 7:
+                    lblFull.TextAlign = ContentAlignment.BottomCenter; break;
+                case 8:
+                    lblFull.TextAlign = ContentAlignment.BottomRight; break;
+            }
         }
     }
 }
