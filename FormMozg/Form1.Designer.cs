638af7fbf5a1c4944dddde1f788658b0dcc5fbf2
Sándor Péter Máró <2021298@dszcberegszaszi.hu>
2022-05-30 12:49:35 +0200

    FormMozog

---
+++ FormMozg/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace FormMozg
+{
+    partial class FormMozgatas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMozgatas));
+            this.kozep = new System.Windows.Forms.Button();
+            this.le = new System.Windows.Forms.Button();
+            this.fel = new System.Windows.Forms.Button();
+            this.balra = new System.Windows.Forms.Button();
+            this.jobbra = new System.Windows.Forms.Button();
+            this.jobbSzel = new System.Windows.Forms.Button();
+            this.meretCsokk = new System.Windows.Forms.Button();
+            this.meretNo = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.atlNo = new System.Windows.Forms.Button();
+            this.atlCsokk = new System.Windows.Forms.Button();
+            this.balSzel = new System.Windows.Forms.Button();
+            this.lent = new System.Windows.Forms.Button();
+            this.fent = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // kozep
+            // 
+            this.kozep.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.kozep.Location = new System.Drawing.Point(364, 189);
+            this.kozep.Name = "kozep";
+            this.kozep.Size = new System.Drawing.Size(75, 23);
+            this.kozep.TabIndex = 0;
+            this.kozep.Text = "Közép";
+            this.kozep.UseVisualStyleBackColor = false;
+            // 
+            // le
+            // 
+            this.le.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.le.Location = new System.Drawing.Point(364, 218);
+            this.le.Name = "le";
+            this.le.Size = new System.Drawing.Size(75, 23);
+            this.le.TabIndex = 1;
+            this.le.Text = "Le";
+            this.le.UseVisualStyleBackColor = false;
+            // 
+            // fel
+            // 
+            this.fel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.fel.Location = new System.Drawing.Point(364, 160);
+            this.fel.Name = "fel";
+            this.fel.Size = new System.Drawing.Size(75, 23);
+            this.fel.TabIndex = 4;
+            this.fel.Text = "Fel";
+            this.fel.UseVisualStyleBackColor = false;
+            // 
+            // balra
+            // 
+            this.balra.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.balra.Location = new System.Drawing.Point(283, 189);
+            this.balra.Name = "balra";
+            this.balra.Size = new System.Drawing.Size(75, 23);
+            this.balra.TabIndex = 5;
+            this.balra.Text = "Balra";
+            this.balra.UseVisualStyleBackColor = false;
+            // 
+            // jobbra
+            // 
+            this.jobbra.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.jobbra.Location = new System.Drawing.Point(445, 189);
+            this.jobbra.Name = "jobbra";
+            this.jobbra.Size = new System.Drawing.Size(75, 23);
+            this.jobbra.TabIndex = 6;
+            this.jobbra.Text = "Jobbra";
+            this.jobbra.UseVisualStyleBackColor = false;
+            // 
+            // jobbSzel
+            // 
+            this.jobbSzel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.jobbSzel.Location = new System.Drawing.Point(526, 189);
+            this.jobbSzel.Name = "jobbSzel";
+            this.jobbSzel.Size = new System.Drawing.Size(75, 23);
+            this.jobbSzel.TabIndex = 7;
+            this.jobbSzel.Text = "Teljesen";
+            this.jobbSzel.UseVisualStyleBackColor = false;
+            this.jobbSzel.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // meretCsokk
+            // 
+            this.meretCsokk.BackColor = System.Drawing.Color.OrangeRed;
+            this.meretCsokk.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.meretCsokk.Location = new System.Drawing.Point(119, 37);
+            this.meretCsokk.Name = "meretCsokk";
+            this.meretCsokk.Size = new System.Drawing.Size(75, 23);
+            this.meretCsokk.TabIndex = 9;
+            this.meretCsokk.Text = "Csökken";
+            this.meretCsokk.UseVisualStyleBackColor = false;
+            this.meretCsokk.UseWaitCursor = true;
+            // 
+            // meretNo
+            // 
+            this.meretNo.BackColor = System.Drawing.Color.Gold;
+            this.meretNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.meretNo.Location = new System.Drawing.Point(38, 37);
+            this.meretNo.Name = "meretNo";
+            this.meretNo.Size = new System.Drawing.Size(75, 23);
+            this.meretNo.TabIndex = 10;
+            this.meretNo.Text = "Növekszik";
+            this.meretNo.UseVisualStyleBackColor = false;
+            this.meretNo.UseWaitCursor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Red;
+            this.groupBox1.Controls.Add(this.atlCsokk);
+            this.groupBox1.Controls.Add(this.atlNo);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.groupBox1.Location = new System.Drawing.Point(283, 292);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(237, 83);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Átlátszóság:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Red;
+            this.groupBox2.Controls.Add(this.meretCsokk);
+            this.groupBox2.Controls.Add(this.meretNo);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.groupBox2.Location = new System.Drawing.Point(283, 31);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(237, 76);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Méret:";
+            this.groupBox2.UseWaitCursor = true;
+            // 
+            // atlNo
+            // 
+            this.atlNo.BackColor = System.Drawing.Color.Gold;
+            this.atlNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.atlNo.Location = new System.Drawing.Point(38, 48);
+            this.atlNo.Name = "atlNo";
+            this.atlNo.Size = new System.Drawing.Size(75, 23);
+            this.atlNo.TabIndex = 11;
+            this.atlNo.Text = "Növekszik";
+            this.atlNo.UseVisualStyleBackColor = false;
+            // 
+            // atlCsokk
+            // 
+            this.atlCsokk.BackColor = System.Drawing.Color.OrangeRed;
+            this.atlCsokk.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.atlCsokk.Location = new System.Drawing.Point(119, 48);
+            this.atlCsokk.Name = "atlCsokk";
+            this.atlCsokk.Size = new System.Drawing.Size(75, 23);
+            this.atlCsokk.TabIndex = 11;
+            this.atlCsokk.Text = "Csökken";
+            this.atlCsokk.UseVisualStyleBackColor = false;
+            // 
+            // balSzel
+            // 
+            this.balSzel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.balSzel.Location = new System.Drawing.Point(202, 189);
+            this.balSzel.Name = "balSzel";
+            this.balSzel.Size = new System.Drawing.Size(75, 23);
+            this.balSzel.TabIndex = 15;
+            this.balSzel.Text = "Teljesen";
+            this.balSzel.UseVisualStyleBackColor = false;
+            // 
+            // lent
+            // 
+            this.lent.BackColor = System.Drawing.Color.Gray;
+            this.lent.Location = new System.Drawing.Point(364, 247);
+            this.lent.Name = "lent";
+            this.lent.Size = new System.Drawing.Size(75, 23);
+            this.lent.TabIndex = 16;
+            this.lent.Text = "Teljesen";
+            this.lent.UseVisualStyleBackColor = false;
+            // 
+            // fent
+            // 
+            this.fent.BackColor = System.Drawing.Color.Gray;
+            this.fent.Location = new System.Drawing.Point(364, 131);
+            this.fent.Name = "fent";
+            this.fent.Size = new System.Drawing.Size(75, 23);
+            this.fent.TabIndex = 17;
+            this.fent.Text = "Teljesen";
+            this.fent.UseVisualStyleBackColor = false;
+            // 
+            // FormMozgatas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.fent);
+            this.Controls.Add(this.lent);
+            this.Controls.Add(this.balSzel);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.jobbSzel);
+            this.Controls.Add(this.jobbra);
+            this.Controls.Add(this.balra);
+            this.Controls.Add(this.fel);
+            this.Controls.Add(this.le);
+            this.Controls.Add(this.kozep);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMozgatas";
+            this.Text = "Form mozgatása";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button kozep;
+        private System.Windows.Forms.Button le;
+        private System.Windows.Forms.Button fel;
+        private System.Windows.Forms.Button balra;
+        private System.Windows.Forms.Button jobbra;
+        private System.Windows.Forms.Button jobbSzel;
+        private System.Windows.Forms.Button meretCsokk;
+        private System.Windows.Forms.Button meretNo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button atlCsokk;
+        private System.Windows.Forms.Button atlNo;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button balSzel;
+        private System.Windows.Forms.Button lent;
+        private System.Windows.Forms.Button fent;
+    }
+}
+
