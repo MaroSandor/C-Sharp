f1bd625e3565978355722743403b784bea1d0d38
Sándor Péter Máró <2021298@dszcberegszaszi.hu>
2022-06-01 10:50:38 +0200

    Kepforog

---
+++ Kepforog/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace Kepforog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.jobbraforog = new System.Windows.Forms.Button();
+            this.Lb_1 = new System.Windows.Forms.PictureBox();
+            this.Lb_8 = new System.Windows.Forms.PictureBox();
+            this.Lb_7 = new System.Windows.Forms.PictureBox();
+            this.Lb_3 = new System.Windows.Forms.PictureBox();
+            this.Lb_2 = new System.Windows.Forms.PictureBox();
+            this.Lb_6 = new System.Windows.Forms.PictureBox();
+            this.Lb_4 = new System.Windows.Forms.PictureBox();
+            this.Lb_5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Lb_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lb_8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lb_7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lb_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lb_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lb_6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lb_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lb_5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Image = global::Kepforog.Properties.Resources.lblb;
+            this.button1.Location = new System.Drawing.Point(61, 47);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(29, 42);
+            this.button1.TabIndex = 1;
+            this.button1.UseMnemonic = false;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // jobbraforog
+            // 
+            this.jobbraforog.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.jobbraforog.Image = global::Kepforog.Properties.Resources.lblj;
+            this.jobbraforog.Location = new System.Drawing.Point(672, 47);
+            this.jobbraforog.Margin = new System.Windows.Forms.Padding(0);
+            this.jobbraforog.Name = "jobbraforog";
+            this.jobbraforog.Size = new System.Drawing.Size(29, 42);
+            this.jobbraforog.TabIndex = 0;
+            this.jobbraforog.UseMnemonic = false;
+            this.jobbraforog.UseVisualStyleBackColor = false;
+            this.jobbraforog.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Lb_1
+            // 
+            this.Lb_1.Image = global::Kepforog.Properties.Resources.lbl1;
+            this.Lb_1.Location = new System.Drawing.Point(376, 22);
+            this.Lb_1.Name = "Lb_1";
+            this.Lb_1.Size = new System.Drawing.Size(67, 67);
+            this.Lb_1.TabIndex = 2;
+            this.Lb_1.TabStop = false;
+            // 
+            // Lb_8
+            // 
+            this.Lb_8.Image = global::Kepforog.Properties.Resources.lbl1;
+            this.Lb_8.Location = new System.Drawing.Point(449, 91);
+            this.Lb_8.Name = "Lb_8";
+            this.Lb_8.Size = new System.Drawing.Size(67, 67);
+            this.Lb_8.TabIndex = 3;
+            this.Lb_8.TabStop = false;
+            // 
+            // Lb_7
+            // 
+            this.Lb_7.Image = global::Kepforog.Properties.Resources.lbl1;
+            this.Lb_7.Location = new System.Drawing.Point(512, 164);
+            this.Lb_7.Name = "Lb_7";
+            this.Lb_7.Size = new System.Drawing.Size(67, 67);
+            this.Lb_7.TabIndex = 4;
+            this.Lb_7.TabStop = false;
+            // 
+            // Lb_3
+            // 
+            this.Lb_3.Image = global::Kepforog.Properties.Resources.lbl1;
+            this.Lb_3.Location = new System.Drawing.Point(242, 164);
+            this.Lb_3.Name = "Lb_3";
+            this.Lb_3.Size = new System.Drawing.Size(67, 67);
+            this.Lb_3.TabIndex = 5;
+            this.Lb_3.TabStop = false;
+            // 
+            // Lb_2
+            // 
+            this.Lb_2.Image = global::Kepforog.Properties.Resources.lbl1;
+            this.Lb_2.Location = new System.Drawing.Point(303, 91);
+            this.Lb_2.Name = "Lb_2";
+            this.Lb_2.Size = new System.Drawing.Size(67, 67);
+            this.Lb_2.TabIndex = 6;
+            this.Lb_2.TabStop = false;
+            // 
+            // Lb_6
+            // 
+            this.Lb_6.Image = global::Kepforog.Properties.Resources.lbl1;
+            this.Lb_6.Location = new System.Drawing.Point(449, 237);
+            this.Lb_6.Name = "Lb_6";
+            this.Lb_6.Size = new System.Drawing.Size(67, 67);
+            this.Lb_6.TabIndex = 7;
+            this.Lb_6.TabStop = false;
+            // 
+            // Lb_4
+            // 
+            this.Lb_4.Image = global::Kepforog.Properties.Resources.lbl1;
+            this.Lb_4.Location = new System.Drawing.Point(303, 237);
+            this.Lb_4.Name = "Lb_4";
+            this.Lb_4.Size = new System.Drawing.Size(67, 67);
+            this.Lb_4.TabIndex = 8;
+            this.Lb_4.TabStop = false;
+            // 
+            // Lb_5
+            // 
+            this.Lb_5.Image = global::Kepforog.Properties.Resources.lbl1;
+            this.Lb_5.Location = new System.Drawing.Point(376, 299);
+            this.Lb_5.Name = "Lb_5";
+            this.Lb_5.Size = new System.Drawing.Size(67, 67);
+            this.Lb_5.TabIndex = 9;
+            this.Lb_5.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Lb_5);
+            this.Controls.Add(this.Lb_4);
+            this.Controls.Add(this.Lb_6);
+            this.Controls.Add(this.Lb_2);
+            this.Controls.Add(this.Lb_3);
+            this.Controls.Add(this.Lb_7);
+            this.Controls.Add(this.Lb_8);
+            this.Controls.Add(this.Lb_1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.jobbraforog);
+            this.Name = "Form1";
+            this.Text = "Kép forgatás";
+            ((System.ComponentModel.ISupportInitialize)(this.Lb_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lb_8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lb_7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lb_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lb_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lb_6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lb_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lb_5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button jobbraforog;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox Lb_1;
+        private System.Windows.Forms.PictureBox Lb_8;
+        private System.Windows.Forms.PictureBox Lb_7;
+        private System.Windows.Forms.PictureBox Lb_3;
+        private System.Windows.Forms.PictureBox Lb_2;
+        private System.Windows.Forms.PictureBox Lb_6;
+        private System.Windows.Forms.PictureBox Lb_4;
+        private System.Windows.Forms.PictureBox Lb_5;
+    }
+}
+
---
+++ Kepforog/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kepforog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void balraforog_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Image s = Lb_1.Image;
+            Lb_1.Image = Lb_2.Image;
+            Lb_2.Image = Lb_3.Image;
+            Lb_3.Image = Lb_4.Image;
+            Lb_4.Image = Lb_5.Image;
+            Lb_5.Image = Lb_6.Image;
+            Lb_6.Image = Lb_7.Image;
+            Lb_7.Image = Lb_8.Image;
+            Lb_8.Image = s;
+        }
+    }
+}
