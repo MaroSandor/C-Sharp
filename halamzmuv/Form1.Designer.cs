a3abd1da4d75094b99ecde2be6617c90fdd892d8
Sándor Péter Máró <2021298@dszcberegszaszi.hu>
2022-06-01 12:51:29 +0200

    halmazmuv

---
+++ halamzmuv/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿
+namespace halamzmuv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listA = new System.Windows.Forms.ListBox();
+            this.listAB = new System.Windows.Forms.ListBox();
+            this.listBA = new System.Windows.Forms.ListBox();
+            this.listMetszet = new System.Windows.Forms.ListBox();
+            this.listUnio = new System.Windows.Forms.ListBox();
+            this.listB = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.halmazAInput = new System.Windows.Forms.TextBox();
+            this.halmazBInput = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnSzamitas = new System.Windows.Forms.Button();
+            this.hozzaad = new System.Windows.Forms.Button();
+            this.torles = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listA
+            // 
+            this.listA.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.listA.FormattingEnabled = true;
+            this.listA.Location = new System.Drawing.Point(12, 53);
+            this.listA.Name = "listA";
+            this.listA.Size = new System.Drawing.Size(120, 264);
+            this.listA.TabIndex = 0;
+            // 
+            // listAB
+            // 
+            this.listAB.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.listAB.FormattingEnabled = true;
+            this.listAB.Location = new System.Drawing.Point(264, 53);
+            this.listAB.Name = "listAB";
+            this.listAB.Size = new System.Drawing.Size(120, 264);
+            this.listAB.TabIndex = 2;
+            // 
+            // listBA
+            // 
+            this.listBA.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.listBA.FormattingEnabled = true;
+            this.listBA.Location = new System.Drawing.Point(390, 53);
+            this.listBA.Name = "listBA";
+            this.listBA.Size = new System.Drawing.Size(120, 264);
+            this.listBA.TabIndex = 3;
+            // 
+            // listMetszet
+            // 
+            this.listMetszet.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.listMetszet.FormattingEnabled = true;
+            this.listMetszet.Location = new System.Drawing.Point(516, 53);
+            this.listMetszet.Name = "listMetszet";
+            this.listMetszet.Size = new System.Drawing.Size(120, 264);
+            this.listMetszet.TabIndex = 4;
+            // 
+            // listUnio
+            // 
+            this.listUnio.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.listUnio.FormattingEnabled = true;
+            this.listUnio.Location = new System.Drawing.Point(642, 53);
+            this.listUnio.Name = "listUnio";
+            this.listUnio.Size = new System.Drawing.Size(120, 264);
+            this.listUnio.TabIndex = 5;
+            // 
+            // listB
+            // 
+            this.listB.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.listB.FormattingEnabled = true;
+            this.listB.Location = new System.Drawing.Point(138, 53);
+            this.listB.Name = "listB";
+            this.listB.Size = new System.Drawing.Size(120, 264);
+            this.listB.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(9, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "A halmaz";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(135, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "B halmaz";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(261, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "A\\B halmaz";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(387, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "B\\A halmaz";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(513, 27);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Metszet";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(639, 27);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Unió";
+            // 
+            // halmazAInput
+            // 
+            this.halmazAInput.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.halmazAInput.Location = new System.Drawing.Point(12, 380);
+            this.halmazAInput.Name = "halmazAInput";
+            this.halmazAInput.Size = new System.Drawing.Size(120, 20);
+            this.halmazAInput.TabIndex = 13;
+            // 
+            // halmazBInput
+            // 
+            this.halmazBInput.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.halmazBInput.Location = new System.Drawing.Point(138, 380);
+            this.halmazBInput.Name = "halmazBInput";
+            this.halmazBInput.Size = new System.Drawing.Size(120, 20);
+            this.halmazBInput.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(9, 364);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "A halmaz";
+            // 
+            // label8
+            // 
+            this.label8.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(135, 364);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "B halmaz";
+            // 
+            // btnSzamitas
+            // 
+            this.btnSzamitas.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnSzamitas.Location = new System.Drawing.Point(264, 378);
+            this.btnSzamitas.Name = "btnSzamitas";
+            this.btnSzamitas.Size = new System.Drawing.Size(75, 23);
+            this.btnSzamitas.TabIndex = 17;
+            this.btnSzamitas.Text = "Elvégez!";
+            this.btnSzamitas.UseVisualStyleBackColor = true;
+            this.btnSzamitas.Click += new System.EventHandler(this.btnSzamitas_Click);
+            // 
+            // hozzaad
+            // 
+            this.hozzaad.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.hozzaad.Location = new System.Drawing.Point(264, 407);
+            this.hozzaad.Name = "hozzaad";
+            this.hozzaad.Size = new System.Drawing.Size(75, 23);
+            this.hozzaad.TabIndex = 18;
+            this.hozzaad.Text = "Hozzáad";
+            this.hozzaad.UseVisualStyleBackColor = true;
+            this.hozzaad.Click += new System.EventHandler(this.hozzaad_Click);
+            // 
+            // torles
+            // 
+            this.torles.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.torles.Location = new System.Drawing.Point(183, 406);
+            this.torles.Name = "torles";
+            this.torles.Size = new System.Drawing.Size(75, 23);
+            this.torles.TabIndex = 19;
+            this.torles.Text = "Töröl";
+            this.torles.UseVisualStyleBackColor = true;
+            this.torles.Click += new System.EventHandler(this.torles_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 440);
+            this.Controls.Add(this.torles);
+            this.Controls.Add(this.hozzaad);
+            this.Controls.Add(this.btnSzamitas);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.halmazBInput);
+            this.Controls.Add(this.halmazAInput);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listB);
+            this.Controls.Add(this.listUnio);
+            this.Controls.Add(this.listMetszet);
+            this.Controls.Add(this.listBA);
+            this.Controls.Add(this.listAB);
+            this.Controls.Add(this.listA);
+            this.Name = "Form1";
+            this.Text = "Halmazok és műveletek";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listA;
+        private System.Windows.Forms.ListBox listAB;
+        private System.Windows.Forms.ListBox listBA;
+        private System.Windows.Forms.ListBox listMetszet;
+        private System.Windows.Forms.ListBox listUnio;
+        private System.Windows.Forms.ListBox listB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox halmazAInput;
+        private System.Windows.Forms.TextBox halmazBInput;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnSzamitas;
+        private System.Windows.Forms.Button hozzaad;
+        private System.Windows.Forms.Button torles;
+    }
+}
+
---
+++ halamzmuv/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace halamzmuv
+{
+    public partial class Form1 : Form
+    {
+        Random szamok = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSzamitas_Click(object sender, EventArgs e)
+        {
+            listA.Items.Clear();
+            listB.Items.Clear();
+
+            int a = Convert.ToInt32(halmazAInput.Text);
+            for (int i = 0; i < a; i++)
+            {
+                listA.Items.Add(szamok.Next(1, 101));
+            }
+
+            int b = Convert.ToInt32(halmazBInput.Text);
+            for (int i = 0; i < b; i++)
+            {
+                listB.Items.Add(szamok.Next(1, 101));
+            }
+        }
+
+        private void hozzaad_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(halmazAInput.Text);
+            for (int i = 0; i < a; i++)
+            {
+                listA.Items.Add(szamok.Next(1, 101));
+            }
+
+            int b = Convert.ToInt32(halmazBInput.Text);
+            for (int i = 0; i < b; i++)
+            {
+                listB.Items.Add(szamok.Next(1, 101));
+            }
+        }
+
+        private void torles_Click(object sender, EventArgs e)
+        {
+            listA.Items.Clear();
+            listB.Items.Clear();
+        }
+    }
+}
