c99100e2a7f829cc27e0774047f30020213281c9
MaroSandor <marosandor2001@gmail.com>
2022-06-09 12:33:35 +0200

    TeglaTDD and Test DONE

--- TeglaTDD/Tegla.cs
+++ TeglaTDD/Tegla.cs
@@ -8,6 +8,30 @@ namespace TeglaTDD
 {
     public class Tegla
     {
-        public Tegla() { }
+        private int szel,
+            hossz,
+            mag;
+
+        public void setTegla(int szel, int hossz, int mag)
+        {
+            this.szel = szel;
+            this.hossz = hossz;
+            this.mag = mag;
+        }
+
+        public Tegla()
+        {
+            // Konstruktor
+        }
+
+        public int getTerfogat()
+        {
+            return szel * hossz * mag;
+        }
+
+        public int getFelszin()
+        {
+            return 2 * (szel * hossz + szel * mag + hossz * mag);
+        }
     }
 }
--- TeglaTDD_Ut/UnitTest1.cs
+++ TeglaTDD_Ut/UnitTest1.cs
@@ -1,14 +1,96 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using TeglaTDD;
 
 namespace TeglaTDD_Ut
 {
     [TestClass]
     public class UnitTest1
     {
+        int vart,
+            kapott;
+
+        Tegla tegla = new Tegla();
+
         [TestMethod]
-        public void TestMethod1()
+        public void Terfogat()
         {
+            // Arrange
+            tegla.setTegla(1, 2, 3);
+
+            vart = 6;
+
+            // Act
+            kapott = tegla.getTerfogat();
+
+            // Assert
+            Assert.AreEqual(vart, kapott);
+        }
+
+        [TestMethod]
+        public void Terfogat2()
+        {
+            // Arrange
+            tegla.setTegla(4, 2, 3);
+
+            vart = 24;
+
+            // Act
+            kapott = tegla.getTerfogat();
+
+            // Assert
+            Assert.AreEqual(vart, kapott);
+        }
+
+        [TestMethod]
+        public void TerfogatR()
+        {
+            // Arrange
+            tegla.setTegla(4, 2, 3);
+
+            vart = 48;
+
+            // Act
+            kapott = tegla.getTerfogat();
+
+            // Assert
+            Assert.AreNotEqual(vart, kapott);
+        }
+
+        [TestMethod]
+        public void Felszin()
+        {
+            tegla.setTegla(1, 2, 3);
+
+            vart = 22;
+
+            kapott = tegla.getFelszin();
+
+            Assert.AreEqual(vart, kapott);
+        }
+
+        [TestMethod]
+        public void Felszin2()
+        {
+            tegla.setTegla(4, 2, 3);
+
+            vart = 52;
+
+            kapott = tegla.getFelszin();
+
+            Assert.AreEqual(vart, kapott);
+        }
+
+        [TestMethod]
+        public void FelszinR()
+        {
+            tegla.setTegla(1, 2, 3);
+
+            vart = 24;
+
+            kapott = tegla.getFelszin();
+
+            Assert.AreNotEqual(vart, kapott);
         }
     }
 }
