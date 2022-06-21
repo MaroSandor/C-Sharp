ac823151b56be3c40a8a30fdcade9b1cb2cd3185
MaroSandor <marosandor2001@gmail.com>
2022-06-21 18:56:18 +0200

    HomersekletForm - YouTube

---
+++ HomersekletForm/HomersekletClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomersekletForm
+{
+    internal class HomersekletClass
+    {
+        private string nev;
+        private int kHom,
+            minHom,
+            maxHom;
+
+        // Konstruktor
+        public HomClass(string sor)
+        {
+            string[] d = sor.Split(';');
+            nev = d[0];
+            kHom = Convert.ToInt32(d[1]);
+            maxHom = Convert.ToInt32(d[2]);
+            minHom = kHom - (maxHom - kHom);
+        }
+    }
+}
